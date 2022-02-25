5ddd02b9885e041b73d2a1fe9e00b8853e9399de
Martin Clauß <mc@cs.uni-bonn.de>
2022-02-25 16:05:37 +0100

    add script to create new posts

---
+++ new_post.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]
+then
+    echo "usage: $0 <filename> (e.g. test -> will create a test.md file)"
+    exit 1
+fi
+
+year=$(date '+%Y')
+month=$(date '+%m')
+hugo new posts/"${year}/${month}/${1}.md"
+
+static_images_path="static/${year}/${month}"
+
+if [[ ! -e  $static_images_path ]]
+then
+    mkdir -p $static_images_path
+    echo "info: created ${static_images_path} for images"
+else
+    echo "info: you can use ${static_images_path} for images"
+fi
